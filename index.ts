a5ec325f91cd4381ac1c9d1a0345442056fbd41d
semmu <iamsemmu@gmail.com>
2022-10-31 19:58:30 +0100

    add more impressive demo

--- index.ts
+++ index.ts
@@ -11,12 +11,18 @@ let terminal: Terminal = new Terminal(process.stdin, process.stdout);
 // let override = new StyleOverrider(shadow, TextColor.Blue, BackgroundColor.Yellow);
 
 let char = new RandomColorLabel('X');
-let padding = new PaddingWidget(char, 6, 2, new Pixel('-'));
+let padding = new PaddingWidget(char, 16, 2, new Pixel('/'));
 let border = new BorderWidget(padding, TextColor.Red);
 
+let hellothere = new RandomColorLabel('hello there');
+let modaltitle = new RandomColorLabel('funky title')
+let modalcontents = new MultiLine('this\nis\nsome multiline\ntext hellyeah', TextAlignment.Right, TextColor.Blue)
+let modal = new ModalWidget(modalcontents, modaltitle, TextColor.Green);
 // let multiline = new MultiLine('goddamn\nmothafuckin12345\nmulti line',TextAlignment.Right, TextColor.Red, BackgroundColor.Black);
-let canvas = new CanvasWidget(31, 21, new Pixel('C'));
+let canvas = new CanvasWidget(51, 17, new Pixel('.'));
 canvas.addWidget(border, new Position(Math.floor(canvas.width / 2), Math.floor(canvas.height / 2)), CanvasAlignment.Middle);
+canvas.addWidget(hellothere)
+canvas.addWidget(modal, new Position(canvas.width, canvas.height), CanvasAlignment.BottomRight);
 
 terminal.widget = canvas;
 terminal.onResize();
