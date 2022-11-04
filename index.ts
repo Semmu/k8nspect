503a5f5e0b20b990b94572578558dd12e831a955
semmu <iamsemmu@gmail.com>
2022-11-04 22:08:32 +0100

    update index.ts to reflect screenshot demo

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { BorderWidget } from "./src/borderwidget"
-import { Position } from "./src/canvaswidget"
+import { CanvasAlignment, Position } from "./src/canvaswidget"
 import { Clock } from "./src/clock"
 import { ColorsTester } from "./src/colorstester"
 import { Label } from "./src/label"
@@ -22,43 +22,42 @@ const terminal: Terminal = new Terminal(process.stdin, process.stdout)
 // let override = new StyleOverrider(shadow, TextColor.Blue, BackgroundColor.Yellow);
 
 const char = new RandomColorLabel("why am i working on this")
-const padding = new PaddingWidget(char, 12, 3, new Pixel("/"))
+const padding = new PaddingWidget(char, 6, 1, new Pixel("/"))
 const border = new BorderWidget(padding, TextColor.Red)
 
 const hellothere = new RandomColorLabel("hello there")
-const modaltitle = new RandomColorLabel("funky title")
-const modalcontents = new MultiLine("this\nis\nsome multiline\ntext hellyeah\nit also has shadow", TextAlignment.Right, TextColor.Blue)
-const modal = new ModalWidget(modalcontents, modaltitle, TextColor.Green)
+const modaltitle = new Label("Colorful title", TextColor.Blue, BackgroundColor.Black)
+const modalcontents = new MultiLine("this\nis some\nmultiline text.\n\nhellyeah.\n\nit also has shadow!!!", TextAlignment.Right, TextColor.Magenta)
+
+const modal = new ModalWidget(modaltitle, modalcontents, TextColor.Green)
 const shadow = new ShadowWidget(modal)
 const clock = new Clock(TextColor.Blue)
-const clock2 = new Clock(TextColor.Yellow)
 const damn = new MultiLine("i should be working on more important things\nalso write proper tests for these classes", TextAlignment.Left, TextColor.Yellow, BackgroundColor.Default, true, false, new Pixel(""))
 
 
 const modal_colors = new ModalWidget(
-  new ColorsTester(),
-  new Label('Normal'))
+  new Label('Normal'),
+  new ColorsTester())
 
 const modal_colors_dim = new ModalWidget(
-  new ColorsTester(true),
-  new Label("Dim"))
+  new Label("Dim"),
+  new ColorsTester(true))
 
 const modal_colors_inverted = new ModalWidget(
-  new ColorsTester(false, false, true),
-  new Label("Inverted"))
+  new Label("Inverted"),
+  new ColorsTester(false, false, true))
 
 const modal_colors_dim_inverted = new ModalWidget(
-  new ColorsTester(true, false, true),
-  new Label('Dim + Inverted'))
+  new Label('Dim + Inverted'),
+  new ColorsTester(true, false, true))
 
-// let multiline = new MultiLine('goddamn\nmothafuckin12345\nmulti line',TextAlignment.Right, TextColor.Red, BackgroundColor.Black);
+let multiline = new MultiLine('goddamn\nmothafuckin12345\nmulti line',TextAlignment.Right, TextColor.Red, BackgroundColor.Black);
 
-// terminal.addWidget(border, new Position(Math.floor(terminal.width / 2), Math.floor(terminal.height / 2)), CanvasAlignment.Middle)
-// terminal.addWidget(hellothere)
-// terminal.addWidget(shadow, new Position(terminal.width, terminal.height), CanvasAlignment.BottomRight)
-// terminal.addWidget(clock, new Position(terminal.width, 0), CanvasAlignment.TopRight)
-// terminal.addWidget(clock2, new Position(terminal.width, 1), CanvasAlignment.TopRight)
-// terminal.addWidget(damn, new Position(0, terminal.height), CanvasAlignment.BottomLeft)
+terminal.addWidget(border, new Position(terminal.width - 5, Math.floor(terminal.height / 2)), CanvasAlignment.CenterRight)
+terminal.addWidget(hellothere, new Position(1, 1))
+terminal.addWidget(shadow, new Position(terminal.width-1, terminal.height-1), CanvasAlignment.BottomRight)
+terminal.addWidget(clock, new Position(terminal.width-1, 1), CanvasAlignment.TopRight)
+terminal.addWidget(damn, new Position(1, terminal.height-1), CanvasAlignment.BottomLeft)
 
 terminal.addWidget(modal_colors, new Position(6, 3))
 terminal.addWidget(modal_colors_dim, new Position(6+TextColors().length*2+6, 3))
