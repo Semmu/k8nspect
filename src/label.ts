fb5a77d97ca64f3ef958d513fa2d41d7d24cdfcc
semmu <iamsemmu@gmail.com>
2022-11-04 21:00:08 +0100

    add isInverted for pixel and label

--- src/label.ts
+++ src/label.ts
@@ -9,23 +9,26 @@ export class Label extends Widget {
   background: BackgroundColor
   isDim: boolean
   isUnderlined: boolean
+  isInverted: boolean
 
   constructor(text: string,
     color: TextColor = TextColor.Default,
     background: BackgroundColor = BackgroundColor.Default,
     isDim = false,
-    isUnderlined = false) {
+    isUnderlined = false,
+    isInverted = false) {
     super()
     this.text = text
     this.color = color
     this.background = background
     this.isDim = isDim
     this.isUnderlined = isUnderlined
+    this.isInverted = isInverted
   }
 
   render(): Output {
     const pixels = [this.text.split("").map((char) => (
-      new Pixel(char, this.color, this.background, this.isDim, this.isUnderlined)
+      new Pixel(char, this.color, this.background, this.isDim, this.isUnderlined, this.isInverted)
     ))]
 
     return Output.fromPixels(pixels)
--- src/pixel.ts
+++ src/pixel.ts
@@ -6,23 +6,26 @@ export class Pixel {
   background: BackgroundColor
   isDim: boolean
   isUnderlined: boolean
+  isInverted: boolean
 
   constructor(char = "",
     color: TextColor = TextColor.Default,
     background: BackgroundColor = BackgroundColor.Default,
     isDim = false,
-    isUnderlined = false) {
+    isUnderlined = false,
+    isInverted = false) {
     this.char = char
     this.color = color
     this.color = color
     this.background = background
     this.isDim = isDim
     this.isUnderlined = isUnderlined
+    this.isInverted = isInverted
   }
 
   clone(): Pixel {
     // im not sure if this is a "deep enough" clone,
     // not referencing anything same by both instances...
-    return new Pixel(this.char, this.color, this.background, this.isDim, this.isUnderlined)
+    return new Pixel(this.char, this.color, this.background, this.isDim, this.isUnderlined, this.isInverted)
   }
 }
